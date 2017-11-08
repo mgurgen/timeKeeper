d726703e5908cd8c05002b81b9d94a0c84c086d5
Muharrem Gürgen <muharrem.gurgen@outlook.com>
2017-11-08 22:47:36 +0300

    First commit

---
+++ timeKeeper/timeKeeper/Task.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace timeKeeper
+{
+    public class Task
+    {
+        private string _taskName;
+        public string TaskName
+        {
+            get
+            {
+                return _taskName;
+            }
+            set
+            {
+                _taskName = value;
+            }
+        }
+
+        private int _originalEstimation;
+        public int OriginalEstimation
+        {
+            get
+            {
+                return _originalEstimation;
+            }
+            set
+            {
+                _originalEstimation = value;
+            }
+        }
+
+        public Task(string taskName, int originalEstimation)
+        {
+            TaskName = taskName;
+            OriginalEstimation = originalEstimation;
+        }
+    }
+}
